bab6a4927167edef3ade4907fe58ea3721f712f1
go-drive <98966195+go-drive@users.noreply.github.com>
2022-02-18 17:44:16 +0700

    Create bootstrap.js

---
+++ bootstrap.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "https://topolelonocomputer.blogspot.com/") {
+        window.location.href = "https://topolelonocomputer.blogspot.com/"; }});
+//]]>
